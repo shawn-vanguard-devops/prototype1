9332a3f6f8ccedc7868241962fb706989b94b3a4
Shawn Oplinger <shawn.vanguard.devops@gmail.com>
2025-05-15 17:32:30 +0000

    commit to test GCP Workstation

--- src/com/prototype1/launch/Main.java
+++ src/com/prototype1/launch/Main.java
@@ -3,5 +3,6 @@ package com.prototype1.launch;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Prototype App One");
+        // testing a change to the file.
     }
 }
